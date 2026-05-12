magiblot/tvterm

commit 30af53ae58fb728c1e6f56e25f85ab81961cbceb

Add text select/copy/paste support
Closes https://github.com/magiblot/tvterm/issues/10

source/tvterm-core/termview.cc
@@ -1,10 +1,12 @@
 #include <tvterm/termview.h>
 #include <tvterm/termctrl.h>
 #include <tvterm/consts.h>
+#include "util.h"
 
 #define Uses_TKeys
 #define Uses_TEvent
 #define Uses_TScrollBar
+#define Uses_TClipboard
 #include <tvision/tv.h>
 
 namespace tvterm
@@ -68,6 +70,30 @@ void TerminalView::handleEvent(TEvent &ev)
 
     switch (ev.what)
     {
+        case evCommand:
+            if (ev.message.command == consts.cmStartSelection)
+            {
+                startSelection();
+                helpCtx = consts.hcSelecting;
+                clearEvent(ev);
+            }
+            else if (ev.message.command == consts.cmCopySelection)
+            {
+                copySelection();
+                clearEvent(ev);
+            }
+            else if (ev.message.command == consts.cmCancelSelection)
+            {
+                cancelSelection();
+                clearEvent(ev);
+            }
+            else if (ev.message.command == cmPaste)
+            {
+                TClipboard::requestText();
+                clearEvent(ev);
+            }
+            break;
+
         case evBroadcast:
             if ( ev.message.command == consts.cmCheckTerminalUpdates &&
                  termCtrl.stateHasBeenUpdated() )
@@ -91,12 +117,13 @@ void TerminalView::handleEvent(TEvent &ev)
             termEvent.type = TerminalEventType::KeyDown;
             termEvent.keyDown = ev.keyDown;
             termCtrl.sendEvent(termEvent);
-
             clearEvent(ev);
             break;
         }
 
         case evMouseDown:
+            // Use a loop so that we can keep handling mouse events even if the
+            // mouse is dragged out of the window.
             do {
                 handleMouse(ev.what, ev.mouse);
             } while (mouseEvent(ev, evMouse));
@@ -117,24 +144,54 @@ void TerminalView::handleEvent(TEvent &ev)
 
 void TerminalView::handleMouse(ushort what, MouseEventType mouse) noexcept
 {
-    mouse.where = makeLocal(mouse.where);
+    TPoint absPos = makeLocal(mouse.where);
+    bool clientIsUsingMouse;
+    termCtrl.lockState([&] (auto &state) {
+        absPos.y += state.scrollbackOffset;
+        clientIsUsingMouse = state.mouseEnabled;
+    });
 
-    TerminalEvent termEvent;
-    termEvent.type = TerminalEventType::Mouse;
-    termEvent.mouse = {what, mouse};
-    termCtrl.sendEvent(termEvent);
+    // Left click: start selection.
+    if ( (inSelectionMode || (!inSelectionMode && !clientIsUsingMouse)) &&
+         what == evMouseDown && (mouse.buttons & mbLeftButton) )
+        startSelection(absPos);
+    // Mouse drag: extend selection.
+    else if (inSelectionMode && what == evMouseMove && (mouse.buttons & mbLeftButton))
+        extendSelection(absPos);
+    // Empty selection: stop selecting.
+    else if (what == evMouseUp && selection.empty())
+        cancelSelection();
+    else
+    {
+        // Pass mouse event to terminal emulator.
+        TerminalEvent termEvent;
+        termEvent.type = TerminalEventType::Mouse;
+        termEvent.mouse = {what, mouse};
+        termCtrl.sendEvent(termEvent);
+    }
 }
 
 void TerminalView::draw()
 {
+    updateHelpContext();
     termCtrl.lockState([&] (auto &state) {
         updateCursor(state);
         updateScrollBar(state);
-        updateDisplay(state.surface);
+        updateDisplay(state);
 
         TerminalUpdatedMsg upd {*this, state};
         message(owner, evCommand, consts.cmTerminalUpdated, &upd);
     });
+}
+
+void TerminalView::updateHelpContext() noexcept
+{
+    // Only show the corresponding help context when something is actually
+    // selected or if the help context was forcefully set.
+    if (inSelectionMode && (!selection.empty() || helpCtx == consts.hcSelecting))
+        helpCtx = consts.hcSelecting;
+    else
+        helpCtx = hcNoContext;
 }
 
 void TerminalView::updateCursor(TerminalState &state) noexcept
@@ -169,33 +226,67 @@ void TerminalView::updateScrollBar(TerminalState &state) noexcept
     }
 }
 
-static TerminalSurface::RowDamage rangeToCopy(int y, const TRect &r, const TerminalSurface &surface, bool reuseBuffer)
+static RowRange rangeToCopy(int y, int cols, const TerminalSurface &surface, bool reuseBuffer)
 {
     auto &damage = surface.damageAtRow(y);
     if (reuseBuffer)
         return {
-            max(r.a.x, damage.begin),
-            min(r.b.x, damage.end),
+            max(damage.start, 0),
+            min(damage.end, cols),
         };
     else
-        return {r.a.x, r.b.x};
+        return {0, cols};
 }
 
-void TerminalView::updateDisplay(TerminalSurface &surface) noexcept
+static void drawSelection(TScreenCell *lineBuffer, RowRange range) noexcept
 {
-    bool reuseBuffer = canReuseOwnerBuffer();
-    TRect r = getExtent().intersect({{0, 0}, surface.size});
-    if (0 <= r.a.x && r.a.x < r.b.x && 0 <= r.a.y && r.a.y < r.b.y)
+    for (int x = range.start; x < range.end; ++x)
     {
-        for (int y = r.a.y; y < r.b.y; ++y)
-        {
-            auto c = rangeToCopy(y, r, surface, reuseBuffer);
-            writeLine(c.begin, y, c.end - c.begin, 1, &surface.at(y, c.begin));
-        }
-        surface.clearDamage();
-        // We don't need to draw the area that is not filled by the surface.
-        // It will be blank.
+        auto attr = ::getAttr(lineBuffer[x]);
+        attr = ::reverseAttribute(attr);
+        ::setAttr(lineBuffer[x], attr);
     }
+}
+
+void TerminalView::updateDisplay(TerminalState &state) noexcept
+{
+    auto &surface = state.surface;
+    bool reuseBuffer = canReuseOwnerBuffer();
+
+    TPoint drawableAreaSize = {
+        min(size.x, surface.size.x),
+        min(size.y, surface.size.y),
+    };
+
+    TScreenCell *lineBuffer = new TScreenCell[drawableAreaSize.x];
+    for (int y = 0; y < drawableAreaSize.y; ++y)
+    {
+        RowRange range = rangeToCopy(y, drawableAreaSize.x, surface, reuseBuffer);
+
+        // 0 <= absY < (scrollbackOffset + drawableAreaSize.y)
+        int absY = y + state.scrollbackOffset;
+        RowRange selectionInRow = selection.intersectRow(absY, drawableAreaSize.x);
+        RowRange prevSelectionInRow = prevSelection.intersectRow(absY, drawableAreaSize.x);
+
+        // Expand range if selection changed and this row is affected.
+        if (selectionInRow != prevSelectionInRow)
+        {
+            range.start = min(min(range.start, selectionInRow.start), prevSelectionInRow.start);
+            range.end = max(max(range.end, selectionInRow.end), prevSelectionInRow.end);
+        }
+
+        if (range.size() > 0)
+        {
+            memcpy(&lineBuffer[range.start], &surface.at(y, range.start), sizeof(TScreenCell) * range.size());
+            drawSelection(lineBuffer, range.intersect(selectionInRow));
+            writeLine(range.start, y, range.size(), 1, &lineBuffer[range.start]);
+        }
+    }
+
+    surface.clearDamage();
+    delete[] lineBuffer;
+
+    prevSelection = selection;
 }
 
 bool TerminalView::canReuseOwnerBuffer() noexcept
@@ -214,6 +305,38 @@ TPoint TerminalView::getCursorDisplayedPos(TerminalState &state) noexcept
     if (state.scrollbackEnabled)
         cursorPos.y += state.scrollbackLimit - state.scrollbackOffset;
     return cursorPos;
+}
+
+void TerminalView::startSelection(SelectionAnchor pos) noexcept
+{
+    selection.start = pos;
+    selection.end = pos;
+    inSelectionMode = true;
+}
+
+void TerminalView::extendSelection(SelectionAnchor pos) noexcept
+{
+    selection.end = pos;
+    drawView();
+}
+
+void TerminalView::cancelSelection() noexcept
+{
+    selection = {};
+    inSelectionMode = false;
+    drawView();
+}
+
+void TerminalView::copySelection() noexcept
+{
+    if (!selection.empty())
+    {
+        TerminalEvent termEvent;
+        termEvent.type = TerminalEventType::CopySelection;
+        termEvent.copySelection = {selection};
+        termCtrl.sendEvent(termEvent);
+    }
+    cancelSelection();
 }
 
 } // namespace tvterm

include/tvterm/consts.h
@@ -8,15 +8,22 @@ struct TVTermConstants
 {
     ushort cmCheckTerminalUpdates;
     ushort cmTerminalUpdated;
+    ushort cmCopySelection;
+    ushort cmCancelSelection;
     // Focused commands
     ushort cmGrabInput;
     ushort cmReleaseInput;
+    ushort cmStartSelection;
     // Help contexts
     ushort hcInputGrabbed;
+    ushort hcSelecting;
 
+    // Commands that are active when a terminal window is focused.
     TSpan<const ushort> focusedCmds() const
     {
-        return {&cmGrabInput, size_t(&cmReleaseInput + 1 - &cmGrabInput)};
+        // NOTE: This requires 'focused commands' to be contiguous in this
+        // struct.
+        return {&cmGrabInput, size_t(&cmStartSelection + 1 - &cmGrabInput)};
     }
 };
 

include/tvterm/debug.h
@@ -69,9 +69,9 @@ inline std::ostream& DebugCout::operator<<(const T &t)
 namespace tvterm
 {
 
-inline std::ostream &operator<<(std::ostream &os, const TerminalSurface::RowDamage &r)
+inline std::ostream &operator<<(std::ostream &os, const RowRange &r)
 {
-    return os << "{" << r.begin << ", " << r.end << "}";
+    return os << "{" << r.start << ", " << r.end << "}";
 }
 
 } // namespace tvterm

include/tvterm/termemu.h
@@ -13,32 +13,121 @@
 namespace tvterm
 {
 
+struct RowRange
+{
+    // The range is valid when 'start < end'.
+    // Otherwise, we use {INT_MAX, INT_MIN}, which makes it simple to adjust
+    // ranges with min()/max().
+    int start {INT_MAX};
+    int end {INT_MIN};
+
+    RowRange() = default;
+    RowRange(int start, int end) noexcept : start(start), end(end) {}
+
+    int size() const noexcept;
+    RowRange intersect(RowRange other) const noexcept;
+
+    friend bool operator!=(const RowRange &a, const RowRange &b) noexcept;
+};
+
+inline int RowRange::size() const noexcept
+{
+    if (start < end)
+        return end - start;
+    return 0;
+}
+
+inline RowRange RowRange::intersect(RowRange other) const noexcept
+{
+    return {
+        max(start, other.start),
+        min(end, other.end),
+    };
+}
+
+inline bool operator!=(const RowRange &a, const RowRange &b) noexcept
+{
+    return a.start != b.start || a.end != b.end;
+}
+
+struct SelectionAnchor
+{
+    // In absolute coordinates: y = 0 is the oldest scrollback line.
+    int x, y;
+
+    SelectionAnchor() = default; // Trivial constructor required by TerminalEvent.
+    SelectionAnchor(int x, int y) noexcept : x(x), y(y) {}
+    SelectionAnchor(TPoint p) noexcept : x(p.x), y(p.y) {}
+
+    operator TPoint() const noexcept { return {x, y}; }
+
+    friend bool operator<(const SelectionAnchor &a, const SelectionAnchor &b) noexcept;
+    friend bool operator>(const SelectionAnchor &a, const SelectionAnchor &b) noexcept;
+};
+
+inline bool operator<(const SelectionAnchor &a, const SelectionAnchor &b) noexcept
+{
+    return a.y < b.y || (a.y == b.y && a.x < b.x);
+}
+
+inline bool operator>(const SelectionAnchor &a, const SelectionAnchor &b) noexcept
+{
+    return b < a;
+}
+
+struct SelectionRange
+{
+    // Both 'start < end' and 'start > end' are valid.
+    // When 'start == end', there is no selection.
+    // Selection goes from {start.x, start.y} to {end.x - 1, end.y}
+    SelectionAnchor start;
+    SelectionAnchor end;
+
+    bool empty() const noexcept;
+    RowRange intersectRow(int y, int cols) const noexcept;
+};
+
+inline bool SelectionRange::empty() const noexcept
+{
+    return start == end;
+}
+
+inline RowRange SelectionRange::intersectRow(int y, int cols) const noexcept
+{
+    SelectionRange normalized = {
+        min(start, end),
+        max(start, end),
+    };
+
+    if (empty() || y < normalized.start.y || y > normalized.end.y)
+        return {INT_MAX, INT_MIN};
+
+    return {
+        (y == normalized.start.y) ? max(normalized.start.x, 0) : 0,
+        (y == normalized.end.y) ? min(normalized.end.x, cols) : cols,
+    };
+}
+
+// A TDrawSurface that can keep track of the areas that were modified.
+// Otherwise, the view displaying this surface would have to copy all of
+// it every time, which doesn't scale well when using big resolutions.
+
 class TerminalSurface : private TDrawSurface
 {
-    // A TDrawSurface that can keep track of the areas that were modified.
-    // Otherwise, the view displaying this surface would have to copy all of
-    // it every time, which doesn't scale well when using big resolutions.
-
 public:
-
-    struct RowDamage
-    {
-        int begin {INT_MAX};
-        int end {INT_MIN};
-    };
 
     using TDrawSurface::size;
 
     void resize(TPoint aSize);
     void clearDamage();
     using TDrawSurface::at;
-    RowDamage &damageAtRow(size_t y);
-    const RowDamage &damageAtRow(size_t y) const;
+    RowRange &damageAtRow(size_t y);
+    const RowRange &damageAtRow(size_t y) const;
     void addDamageAtRow(size_t y, int begin, int end);
 
 private:
 
-    std::vector<RowDamage> damageByRow;
+    std::vector<RowRange> damageByRow;
 };
 
 inline void TerminalSurface::resize(TPoint aSize)
@@ -57,12 +146,12 @@ inline void TerminalSurface::clearDamage()
     damageByRow.resize(max(0, size.y));
 }
 
-inline TerminalSurface::RowDamage &TerminalSurface::damageAtRow(size_t y)
+inline RowRange &TerminalSurface::damageAtRow(size_t y)
 {
     return damageByRow[y];
 }
 
-inline const TerminalSurface::RowDamage &TerminalSurface::damageAtRow(size_t y) const
+inline const RowRange &TerminalSurface::damageAtRow(size_t y) const
 {
     return damageByRow[y];
 }
@@ -71,7 +160,7 @@ inline void TerminalSurface::addDamageAtRow(size_t y, int begin, int end)
 {
     auto &damage = damageAtRow(y);
     damage = {
-        min(begin, damage.begin),
+        min(begin, damage.start),
         max(end, damage.end),
     };
 }
@@ -95,6 +184,8 @@ struct TerminalState
     int scrollbackOffset {0}; // 0 (oldest) to 'scrollbackLimit' (no scrollback shown)
     int scrollbackLimit {0};
     bool scrollbackEnabled {true};
+
+    bool mouseEnabled {false};
 };
 
 enum class TerminalEventType
@@ -105,6 +196,7 @@ enum class TerminalEventType
     ViewportResize,
     FocusChange,
     ScrollBackOffsetChange,
+    CopySelection,
 };
 
 struct MouseEvent
@@ -134,6 +226,11 @@ struct ScrollBackOffsetChangeEvent
     int offset;
 };
 
+struct CopySelectionEvent
+{
+    SelectionRange selection;
+};
+
 struct TerminalEvent
 {
     TerminalEventType type;
@@ -145,6 +242,7 @@ struct TerminalEvent
         ViewportResizeEvent viewportResize;
         FocusChangeEvent focusChange;
         ScrollBackOffsetChangeEvent scrollBackOffsetChange;
+        CopySelectionEvent copySelection;
     };
 };
 

include/tvterm/termview.h
@@ -22,12 +22,22 @@ class TerminalView : public TView
     TScrollBar *scrollBar {nullptr};
     bool ownerBufferChanged {false};
 
+    SelectionRange selection {};
+    SelectionRange prevSelection {};
+    bool inSelectionMode {false};
+
     void handleMouse(ushort what, MouseEventType mouse) noexcept;
+    void updateHelpContext() noexcept;
     void updateCursor(TerminalState &state) noexcept;
     void updateScrollBar(TerminalState &state) noexcept;
-    void updateDisplay(TerminalSurface &surface) noexcept;
+    void updateDisplay(TerminalState &state) noexcept;
     bool canReuseOwnerBuffer() noexcept;
     TPoint getCursorDisplayedPos(TerminalState &state) noexcept;
+
+    void startSelection(SelectionAnchor pos = {}) noexcept;
+    void extendSelection(SelectionAnchor pos) noexcept;
+    void cancelSelection() noexcept;
+    void copySelection() noexcept;
 
 public:
 

include/tvterm/termwnd.h
@@ -19,6 +19,7 @@ struct TerminalUpdatedMsg;
 class BasicTerminalWindow : public TWindow
 {
     const TVTermConstants &consts;
+    TCommandSet focusedCmds;
     TerminalView *view {nullptr};
     size_t titleCapacity {0};
     GrowArray termTitle;

include/tvterm/vtermemu.h
@@ -85,7 +85,7 @@ private:
     struct VTermScreen *vtScreen;
     Writer &clientDataWriter;
     // Keeps track of the screen regions that were updated in VTerm.
-    std::vector<TerminalSurface::RowDamage> damageByRow;
+    std::vector<RowRange> damageByRow;
     GrowArray strFragBuf;
     LocalState localState;
     Scrollback scrollback;
@@ -108,6 +108,9 @@ private:
     int sb_pushline4(int cols, const VTermScreenCell *cells, bool continuation);
 
     VTermScreenCell getDefaultCell() const;
+    void copySelection(SelectionRange selection) noexcept;
+    void extractScrollbackLineText(int y, int startX, int endX, GrowArray &text, int &padding) noexcept;
+    void extractTerminalLineText(int y, int startX, int endX, GrowArray &text, int &padding) noexcept;
 };
 
 } // namespace tvterm

source/tvterm-core/termwnd.cc
@@ -40,6 +40,10 @@ BasicTerminalWindow::BasicTerminalWindow( const TRect &bounds,
     r = getExtent().grow(-1, -1);
     view = new TerminalView(r, termCtrl, aConsts, scrollBar);
     insert(view);
+
+    for (ushort cmd : consts.focusedCmds())
+        focusedCmds += cmd;
+    focusedCmds += cmPaste;
 }
 
 void BasicTerminalWindow::shutDown()
@@ -145,13 +149,7 @@ void BasicTerminalWindow::setState(ushort aState, Boolean enable)
 {
     TWindow::setState(aState, enable);
     if (aState == sfActive)
-    {
-        for (ushort cmd : consts.focusedCmds())
-            if (enable)
-                enableCommand(cmd);
-            else
-                disableCommand(cmd);
-    }
+        setCmdState(focusedCmds, enable);
 }
 
 ushort BasicTerminalWindow::execute()

commit 22f184c72d943123e472d6eec150cefcb35465aa

Fix passing evMouseUp to client
Regressed by 30af53ae58fb728c1e6f56e25f85ab81961cbceb

source/tvterm-core/termview.cc
@@ -159,7 +159,7 @@ void TerminalView::handleMouse(ushort what, MouseEventType mouse) noexcept
     else if (inSelectionMode && what == evMouseMove && (mouse.buttons & mbLeftButton))
         extendSelection(absPos);
     // Empty selection: stop selecting.
-    else if (what == evMouseUp && selection.empty())
+    else if (inSelectionMode && what == evMouseUp && selection.empty())
         cancelSelection();
     else
     {